fc097e0927f9a472a4b24cb877ad8357496b8ce0
Akshay Srinivasan <35820570+akshays2112@users.noreply.github.com>
2024-02-14 07:52:23 +0530

    Added Grid

--- MauiControlsLibraryExamples/MainPage.xaml.cs
+++ MauiControlsLibraryExamples/MainPage.xaml.cs
@@ -7,6 +7,9 @@
             InitializeComponent();
             TestListbox1.Labels = ["Akshay", "John", "Bob", "Alice", "Jane", "Jill", "Brandon", "Josh", "Allen", "Jake", "Zed",
                 "Kate", "Allison", "Chris", "Mike", "Lauren", "James" ];
+            TestGrid1.HeaderNames = ["First Name", "Middle Name", "Last Name", "Age"];
+            TestGrid1.Data = new string[,] { { "Akshay", "", "Srinivasan", "21" }, { "John", "", "Baptiste", "25" },
+                { "Bob", "Edward", "Smith", "34" }, { "Alice", "Charlotte", "Green", "26" } };
         }
 
         private void TestButton1_OnMCCLButtonTapped(object sender, EventArgs e)
@@ -14,11 +17,20 @@
             TestLabel1.LabelText = "TestButton1";
         }
 
-        private void TestListbox1_OnMCCLListboxTapped(object sender, MauiControlsLibrary.MCCLListbox.ListboxEventArgs e)
+        private void TestListbox1_OnMCCLListboxTapped(object sender, MauiControlsLibrary.MCLListbox.ListboxEventArgs e)
         {
             if (e.CurrentIndex >= 0 && e.CurrentIndex < TestListbox1.Labels.Length)
             {
                 TestLabel1.LabelText = TestListbox1.Labels[e.CurrentIndex];
+            }
+        }
+
+        private void TestGrid1_OnMCLGridTapped(object sender, MauiControlsLibrary.MCLGrid.GridEventArgs e)
+        {
+            if(TestGrid1.Data != null && e.CurrentRowIndex >= 0 && e.CurrentRowIndex < TestGrid1.Data.GetLength(0) && 
+                e.CurrentColIndex >= 0 && e.CurrentColIndex < TestGrid1.Data.GetLength(1))
+            {
+                TestLabel1.LabelText = TestGrid1.Data[e.CurrentRowIndex, e.CurrentColIndex];
             }
         }
     }
